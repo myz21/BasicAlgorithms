499165c7de5bbcf648fa2fc3370a060d81c182a1
Muhammed Yıldız <155391307+myz21@users.noreply.github.com>
2026-01-15 18:44:01 +0300

    0 ms (100%) 42.1 MB (56.73%) Easy 0509-fibonacci-number .java 01-15-2026

--- LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.java
+++ LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.java
@@ -5,14 +5,19 @@ class Solution {
             return n;
         }
 
-        int [] fibb = new int [n+1];
-        fibb[0] = 0;
-        fibb[1] = 1;
+        int prev = 0;
+        int curr = 1;
+        int next = 0;
 
-        for(int i = 2 ; i < n+1; i++){
-            fibb[i] = fibb[i-1]+fibb[i-2];
+        for (int i = 2; i <= n; i++){
+
+            next = prev + curr;
+
+            prev = curr;
+            curr = next;
         }
-        return fibb[n];
+
+        return curr;
 
     }
 }
