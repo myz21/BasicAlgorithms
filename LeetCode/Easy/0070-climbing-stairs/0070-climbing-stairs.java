a54eb44fc1276f49283bbafd113ad74af6db5019
Muhammed Yıldız <155391307+myz21@users.noreply.github.com>
2026-01-15 20:40:37 +0300

    0 ms (100%) 42 MB (59.5%) Easy 0070-climbing-stairs .java 01-15-2026

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,21 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        if ( n == 0){
-            return 1;
+        if (n <= 1) return 1;
+        int[] dp = new int[n + 1];
+        dp[0] = 1; 
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
         }
-
-        if (n < 0){
-            return 0;
-        }
-        int prev = 1;
-        int curr = 1;
-        int next = 0;
-
-        for (int i = 1 ; i <= n ; i++ ){
-            next = curr + prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
+        return dp[n];
     }
 }
