7cdd18b48868d47c03861755abb81e75f7c5bcba
Muhammed Yıldız <155391307+myz21@users.noreply.github.com>
2026-01-16 15:47:18 +0300

    1 ms (99.74%) 46.8 MB (6.76%) Medium 0120-triangle .java 01-16-2026

--- LeetCode/Medium/0120-triangle/0120-triangle.java
+++ LeetCode/Medium/0120-triangle/0120-triangle.java
@@ -1,15 +1,32 @@
 class Solution {
+
+    Integer [][] memo;
+
     public int minimumTotal(List<List<Integer>> triangle) {
+
+        memo = new Integer[triangle.size()][triangle.size()];
+         return solve(0,0,triangle);
+    }
+
+    private int solve(int i, int j, List<List<Integer>> triangle) {
         
-        int [] dp = new int [triangle.size()+1];
+        if(i == triangle.size()){
+            return 0;
+        }
 
-        Arrays.fill(dp,0);
+        if(triangle.size() == 1){
+            return triangle.get(i).get(j);
+        }
 
-         for (int i = triangle.size() - 1; i>= 0 ; i-- ){
-            for(int j = 0; j < triangle.get(i).size(); j++){
-                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j+1]);
-            }
-         }
-         return dp[0];
+        if(memo[i][j] != null){
+            return memo[i][j];
+        }
+
+        int current = triangle.get(i).get(j);
+        int down = solve(i+1,j,triangle);
+        int downRight = solve(i+1,j+1,triangle);
+        memo[i][j] = current + Math.min(down, downRight);
+        return memo[i][j];
+    
     }
 }
